3bd625689bcecbdc3df82bd1ddb867621849e11a
Chaithra L <chaithrachailingaraj@gmail.com>
2026-08-22 17:40:23 +0530

    Initial commit

---
+++ 2nd.py
@@ -0,0 +1,9 @@
+a,b=23,45
+temp=0
+
+temp=a
+a=b
+b=temp
+
+print("a :",a)
+print("b :",b)
---
+++ 4th.py
@@ -0,0 +1,13 @@
+# s=input("String :")
+# print(len(s.replace(" ","")))
+
+# print("Hello\n\tWorld\nThis is a backslash: \\")
+
+# print("https:user\\username\\")
+
+a=int(input("Enter a :"))
+b=int(input("Enter b :"))
+
+print(a>10 and b>10)
+print(a<5 or b<5)
+print(not(a>5))
