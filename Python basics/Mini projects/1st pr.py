3732d8965b5ac25b557d6209e1e0962072b4267b
Chaithra L <chaithrachailingaraj@gmail.com>
2026-08-24 21:58:10 +0530

    NEW project

--- Python basics/Mini projects/1st pr.py
+++ Python basics/Mini projects/1st pr.py
@@ -10,7 +10,9 @@ computer=random.choice(choices)
 if user==computer:
     print(" its Tie 😑")
 
-elif (user == "Rock" and computer == "Scissor") or (user == "Paper" and computer == "Rock") or (user == "Scissor" and computer == "Paper"):
+elif (user == "Rock" and computer == "Scissor") or \
+     (user == "Paper" and computer == "Rock") or \
+     (user == "Scissor" and computer == "Paper"):
      print("You win🔥")
 
 else:
